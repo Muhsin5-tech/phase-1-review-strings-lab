f206d6e542a823ba3bed0a7c04e88c41ea9fc0b1
Muhsin Ali <muhsin.ali@student.moringaschool.com>
2024-09-23 12:03:56 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Muhsin'
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser[0]; 
+const restOfName = currentUser.slice(1);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+const shortGreeting = `Welcome, ${firstInitial}!`
